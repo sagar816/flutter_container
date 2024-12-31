985d765a128c4c2f4b7d1981b544d5b2db463858
sagart <sagart@gmail.com>
2024-12-31 17:43:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# flutter_container
+
+Container - An invisible Box. It is a layout - collection of multiple childs.
+
+Can hold only one child..  but child can hold many childs
+
+Like listviews,row, coloumns, this have many widgets, and collectively if you want store in a box and give collective property to decorate it - background design related, we can use container.
+
+Or if want to put tap - on  many components collectively,  we can put it in a invisible container and use tap on it.
+
+It is a widget that combines common painting, positioning, and sizing of the child widgets.
+
+It is also a class to store one or more widgets and position them on the screen according to our needs
+
+Generally it is similar to box for storing contents
+
+It allows many attributes to the user for decorating its child widget such as using margin, which separates the container with other contents
+
+Container Properties
+- color
+- box decoration
+- box shadow
+
+
+Until - size and colour is not given - container is invisible
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Flutter Container"),
+      ),
+      body: Center(   // Center applied to body area
+        child: Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+          child: Text("Hello Flutter"),
+        ),
+      ), //calling/invoke constructor of container
+    );
+  }
+}
